7c47fb62e69293c03b24c1ac8149a40da63b5302
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-10-15 16:59:44 +0100

    general css tweaks

--- src/components/Calandar/index.js
+++ src/components/Calandar/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { InlineWidget } from "react-calendly";
+import styles from './index.module.css'
 
 export function Calandar({ selectedOption }) {
     const [url, setUrl] = useState("");
@@ -11,9 +12,9 @@ export function Calandar({ selectedOption }) {
     }, [selectedOption])
 
     return (
-        <>
+        <div className={styles.calandar}>
             {selectedOption?.url ? <InlineWidget url={url} LoadingSpinner={"true"} /> : 'loading'}
-        </>
+        </div>
     )
 }
 
