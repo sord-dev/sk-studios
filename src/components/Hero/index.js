6a885232820396131e13bc94f29f03048553e687
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-10-16 14:52:04 +0100

    adding logos [hero error]

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import styles from './index.module.css'; // Import your CSS module
 import Link from 'next/link';
+import Image from 'next/image';
+
+
 
 export const Hero = () => {
     return (
         <header className={styles.hero}>
             <div className={styles.heroContent}>
                 <div className={styles.heroText}>
-                    <h1 className={styles.heroTitle}>SK Studios</h1>
-                    <p className={styles.heroSubtitle}>Wys here my bro</p>
+                    <Image src={"/sk-logo-nbg.png"} width={248} height={248} alt='SK Productions Logo' />
+
+                    <h2>test</h2>
+
                     <Link href={'/book'}><button className={styles.ctaButton}>Book a session</button></Link>
                 </div>
-                <div className={styles.heroContact}>
-                    <Link href={'mailto:skproductions@gmail.com'}><p className={styles.contactNumber}>Contact us: skproductions@gmail.com</p></Link>
-                </div>
+            </div>
+            <div className={styles.heroContact}>
+                <Link href={'mailto:skproductions@gmail.com'}><p className={styles.contactNumber}>Contact us: skproductions@gmail.com</p></Link>
             </div>
         </header>
     );
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './index.module.css'; // Import your CSS module
 import Link from 'next/link';
+import Image from 'next/image';
 
 export const Navbar = () => {
     return (
         <header className={styles.navbar}>
             <div className={styles.container}>
                 <div className={styles.logoSection}>
+                    <Image src={"/sk-logo-nbg.png"} width={78} height={78} alt='SK Productions Logo' />
                     <div className={styles.logo}>
                         <h1 className={styles.logoText}>SK Studios</h1>
                         <p className={styles.subtitle}>Wys here my bro</p>
