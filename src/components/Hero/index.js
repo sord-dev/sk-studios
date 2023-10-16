6e1426441278c4737716648d463a325522dacc43
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-10-16 17:23:13 +0100

    logo variant created

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,16 +3,24 @@ import styles from './index.module.css'; // Import your CSS module
 import Link from 'next/link';
 import Image from 'next/image';
 
-
-
 export const Hero = () => {
     return (
         <header className={styles.hero}>
             <div className={styles.heroContent}>
                 <div className={styles.heroText}>
-                    <Image src={"/sk-logo-nbg.png"} width={248} height={248} alt='SK Productions Logo' />
+                    {/* Set max-width and max-height for the Image component */}
+                    <div className={styles.logoContainer}>
+                        <Image
+                            src={"/sk-logo-nbg.png"}
+                            width={400}
+                            height={400}
+                            alt='SK Productions Logo'
+                            layout="responsive" // Ensures the image maintains aspect ratio within the specified dimensions
+                            draggable='false'
+                        />
+                    </div>
 
-                    <h2>test</h2>
+                    <h1 className={styles.logoText}>SK Studios</h1>
 
                     <Link href={'/book'}><button className={styles.ctaButton}>Book a session</button></Link>
                 </div>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,13 +7,15 @@ export const Navbar = () => {
     return (
         <header className={styles.navbar}>
             <div className={styles.container}>
-                <div className={styles.logoSection}>
-                    <Image src={"/sk-logo-nbg.png"} width={78} height={78} alt='SK Productions Logo' />
-                    <div className={styles.logo}>
-                        <h1 className={styles.logoText}>SK Studios</h1>
-                        <p className={styles.subtitle}>Wys here my bro</p>
+                <Link href={'/'}>
+                    <div className={styles.logoSection}>
+                        <Image src={"/sk-logo-nbg.png"} width={78} height={78} alt='SK Productions Logo' />
+                        <div className={styles.logo}>
+                            <h1 className={styles.logoText}>SK Studios</h1>
+                            <p className={styles.subtitle}>Wys here my bro</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
                 <div className={styles.contactSection}>
                     <Link href={'mailto:skproductions@gmail.com'}><p className={styles.contactNumber}>Contact us: skproductions@gmail.com</p></Link>
                 </div>
